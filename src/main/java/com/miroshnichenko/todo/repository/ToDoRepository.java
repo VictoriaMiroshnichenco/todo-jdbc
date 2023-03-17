7cc17d925e86d7feaf2a254329026d2af5784d2e
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-03-18 00:47:21 +0200

    49 Spring Data REST repository

--- src/main/java/com/miroshnichenko/todo/repository/ToDoRepository.java
+++ src/main/java/com/miroshnichenko/todo/repository/ToDoRepository.java
@@ -2,5 +2,7 @@ package com.miroshnichenko.todo.repository;
 
 import com.miroshnichenko.todo.domain.ToDo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "st")
 public interface ToDoRepository extends  CrudRepository<ToDo,String> {}

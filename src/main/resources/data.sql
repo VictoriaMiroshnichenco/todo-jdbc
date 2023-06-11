cbc93da1c6802e30d521a7f6113599e8d7a3d4ed
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-06-11 19:36:58 +0300

    53 App:change db datasetup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,4 @@ insert into to_do values ('7fd921cfd2b64dc7b995633e8209f385','Buy
 insert into to_do values ('5820a4c2abe74f409da89217bf905a0c','Read a
  Book','2018-09-02 16:00:01','2018-09-02 16:00:01',false);
 insert into to_do values ('a44b6db26aef49e39d1b68622f55c347','Go to Spring
- One 2018','2018-09-18 12:00:00','2018-09-18 12:00:00',false);
+ One 2018','2018-09-18 12:00:00','2018-09-18 12:00:00',true);

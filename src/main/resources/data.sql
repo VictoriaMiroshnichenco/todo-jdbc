52990827586967698d5fec44f7e462b5e487c919
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-08-08 21:50:11 +0300

    55 Datasetup: added more

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 use testdb;
 insert into to_do values ('7fd921cfd2b64dc7b995633e8209f385','Buy
- Milk','2018-09-23 15:00:01','2018-09-23 15:00:01',false);
+ Milk','2022-09-23 15:00:01','2022-09-23 15:00:01',false);
 insert into to_do values ('5820a4c2abe74f409da89217bf905a0c','Read a
- Book','2018-09-02 16:00:01','2018-09-02 16:00:01',false);
-insert into to_do values ('a44b6db26aef49e39d1b68622f55c347','Go to Spring
- One 2018','2018-09-18 12:00:00','2018-09-18 12:00:00',true);
+ Book','2022-09-02 16:00:01','2022-09-02 16:00:01',false);
+insert into to_do values ('a44b6db26aef49e39d1b68622f55c347','Learn Spring Boot','2022-09-18 12:00:00','2022-09-18 12:00:00',true);
+insert into to_do values ('26ae6db26aef49e39d1b68622f55c343','Win a war!','2022-09-18 12:00:00','2022-09-18 12:00:00',false);

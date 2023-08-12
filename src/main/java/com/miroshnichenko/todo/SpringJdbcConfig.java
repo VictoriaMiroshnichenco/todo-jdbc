20f9818eabc556c6be730a59fd719a7052ce2984
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-08-13 00:10:11 +0300

    60 Database: added task owner and employee table. removed jpa deps

---
+++ src/main/java/com/miroshnichenko/todo/SpringJdbcConfig.java
@@ -0,0 +1,23 @@
+package com.miroshnichenko.todo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+
+//@Configuration
+public class SpringJdbcConfig {
+    //commented out as i use properties configuration
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springjdbc");
+        dataSource.setUsername("guest_user");
+        dataSource.setPassword("guest_password");
+
+        return dataSource;
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,26 @@
 DROP TABLE IF EXISTS to_do;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS task_status;
+CREATE TABLE employee
+(
+    id varchar(36) not null primary key,
+    name varchar(255) not null,
+    about varchar(255),
+    created timestamp,
+    modified timestamp,
+    active boolean
+);
 CREATE TABLE to_do
 (
-  id varchar(36) not null primary key,
+  id varchar(36)  not null , primary key(id),
   description varchar(255) not null,
   created timestamp,
   modified timestamp,
   completed boolean,
-  taskstatus varchar(255)
+  taskstatus varchar(255),
+  ownerid varchar(36),
+  FOREIGN KEY (ownerid) REFERENCES employee(id)
+
 );
 DROP TABLE IF EXISTS task_status;
 CREATE TABLE task_status
@@ -17,3 +31,5 @@ CREATE TABLE task_status
     modified timestamp,
     active boolean
 );
+
+

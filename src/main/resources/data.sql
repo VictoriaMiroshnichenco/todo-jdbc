5fc328d4f0a5c9e99874b011bbc97685d94256a8
Victoria.Miroshnichenco <mvvmvvmvv@gmail.com>
2023-08-12 19:16:30 +0300

    57 Database: added some comments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ insert into to_do values ('7fd921cfd2b64dc7b995633e8209f385','Buy
  Milk','2022-09-23 15:00:01','2022-09-23 15:00:01',false,'Defined');
 insert into to_do values ('5820a4c2abe74f409da89217bf905a0c','Read a
  Book','2022-09-02 16:00:01','2022-09-02 16:00:01',false,'Defined');
-insert into to_do values ('a44b6db26aef49e39d1b68622f55c347','Learn Spring Boot','2022-09-18 12:00:00','2022-09-18 12:00:00',true,'Done');
+insert into to_do values ('a44b6db26aef49e39d1b68622f55c347','Learn Spring Boot','2022-09-18 12:00:00','2022-09-18 12:00:00',true,'In-progress');
 insert into to_do values ('26ae6db26aef49e39d1b68622f55c343','Win a war!','2022-09-18 12:00:00','2022-09-18 12:00:00',false,'In-progress');
 
 
